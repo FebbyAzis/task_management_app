bd5681995773e80be09c85baf9203b4d1114a970
Febby Azis <febbyazis02@gmail.com>
2022-12-04 07:39:10 +0700

    commit video 13

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:task_management_app/app/data/controller/auth_controller.dart';
 import 'package:unicons/unicons.dart';
 
 import 'package:task_management_app/app/routes/app_pages.dart';
@@ -10,102 +11,125 @@ import 'package:task_management_app/app/routes/app_pages.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
+  final authC = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[100],
       body: Container(
-      margin: context.isPhone
-      ?EdgeInsets.all(Get.width * 0.1)
-      :EdgeInsets.all(Get.height * 0.1),
-      decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(50),
-      color: Colors.white,
-      ),
-      child: Row(children: [
-        //biru
-        !context.isPhone?
-        Expanded(
-          //putih
-          child: Container(
-          decoration: const BoxDecoration(
-           borderRadius: BorderRadius.only(
-           topLeft: Radius.circular(50),
-           bottomLeft: Radius.circular(50),
-          ),
-          color: Colors.blue,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const  [
-                Text('Welcome', style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 70,
-                ),
-                ),
-                Text('Please Sign In', style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 30,
-                ),),
-                Text('Start Journey With Us',
-                 style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 20,
-                ),),
-              ],),
-            ),
-          ),
-          ):const SizedBox(),
+        margin: context.isPhone
+            ? EdgeInsets.all(Get.width * 0.1)
+            : EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            //biru
+            !context.isPhone
+                ? Expanded(
+                    //putih
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          bottomLeft: Radius.circular(50),
+                        ),
+                        color: Colors.blue,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              'Welcome',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 70,
+                              ),
+                            ),
+                            Text(
+                              'Please Sign In',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                              ),
+                            ),
+                            Text(
+                              'Start Journey With Us',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                : const SizedBox(),
 
-          Expanded(
-          child: Container(
-          decoration: const BoxDecoration(
-           borderRadius: BorderRadius.only(
-           topRight: Radius.circular(50),
-           bottomRight: Radius.circular(50),
-          ),
-        ),
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        context.isPhone
-        ?Column(
-                //crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const  [
-                Text('Welcome', style: TextStyle(
-                  color: Colors.grey, 
-                  fontSize: 40,
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
                 ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    context.isPhone
+                        ? Column(
+                            //crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                'Welcome',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 40,
+                                ),
+                              ),
+                              Text(
+                                'Please Sign In',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text(
+                                'Start Journey With Us',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          )
+                        : const SizedBox(),
+                    Image.asset(
+                      'assets/images/login.png',
+                      height: Get.height * 0.5,
+                    ),
+                    FloatingActionButton.extended(
+                      onPressed: () => authC.signInWithGoogle(),
+                      label: const Text('Sign In With  Google'),
+                      icon: const Icon(
+                        UniconsLine.google,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
                 ),
-                Text('Please Sign In', style: TextStyle(
-                  color: Colors.grey, 
-                  fontSize: 20,
-                ),),
-                Text('Start Journey With Us',
-                 style: TextStyle(
-                  color: Colors.grey, 
-                  fontSize: 15,
-                ),),
-              ]): const SizedBox(),
-
-        Image.asset(
-        'assets/images/login.png',
-        height: Get.height * 0.5,
+              ),
+            )
+          ],
         ),
-        FloatingActionButton.extended(
-        onPressed: () => Get.toNamed(Routes.HOME), 
-        label: const Text('Sign In With  Google' ),
-        icon: const Icon(UniconsLine.google, color: Colors.white,),
-        )
-        ]),
-        ),
-        )
-      ]),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,36 @@
+// import 'dart:js';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'app/routes/app_pages.dart';
+import 'package:task_management_app/app/data/controller/auth_controller.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
 import 'firebase_options.dart';
 
 
 
-Future <void> main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
   );  
-  runApp(
-    GetMaterialApp(
+  Get.put(AuthController(), permanent: true);
+  runApp(StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot){
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: snapshot.data != null ? Routes.HOME: Routes.LOGIN ,
       getPages: AppPages.routes,
-    ),
+        );
+      
+    },)
+    
   );
 }
